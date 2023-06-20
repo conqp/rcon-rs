9e744ccae838ffba694c47e710cd8e27d1e68ec9
Richard Neumann <mail@richard-neumann.de>
2023-06-20 14:50:20 +0200

    Implement communicate()

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,16 @@ use std::net::TcpStream;
 
 pub fn rcon(host: &str, passwd: &str, command: &[&str]) -> Result<String, Either<Error, String>> {
     let mut stream = TcpStream::connect(host).map_err(Left)?;
-    let login: Vec<u8> = Packet::from(passwd).into();
-    stream.write(&login).map_err(Left)?;
-    let _ = Packet::try_from(&mut stream)?;
-    let command: Vec<u8> = Packet::from(command).into();
-    stream.write(&command).map_err(Left)?;
-    let command_response = Packet::try_from(&mut stream)?;
+    communicate(&mut stream, Packet::from(passwd))?;
+    let command_response = communicate(&mut stream, Packet::from(command))?;
     Ok(command_response.text())
+}
+
+pub fn communicate(
+    stream: &mut TcpStream,
+    packet: Packet,
+) -> Result<Packet, Either<Error, String>> {
+    let bytes: Vec<u8> = packet.into();
+    stream.write(bytes.as_slice()).map_err(Left)?;
+    Packet::try_from(stream)
 }
