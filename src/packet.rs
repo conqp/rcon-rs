d74fcd65364ffaa77769a1ca14201949f83ace9a
Richard Neumann <mail@richard-neumann.de>
2023-06-20 19:43:39 +0200

    Do not update type

--- src/packet.rs
+++ src/packet.rs
@@ -48,7 +48,6 @@ impl Packet {
 
 impl AddAssign for Packet {
     fn add_assign(&mut self, rhs: Self) {
-        self.typ = rhs.typ;
         self.payload.extend_from_slice(&rhs.payload);
     }
 }
