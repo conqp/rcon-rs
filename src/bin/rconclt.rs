820e7434799ff94a03c6a8f7119ed61a871bb8af
Richard Neumann <mail@richard-neumann.de>
2024-03-01 19:08:50 +0100

    Add timeout option

--- src/bin/rconclt.rs
+++ src/bin/rconclt.rs
@@ -2,12 +2,16 @@ use clap::Parser;
 use log::error;
 use rcon::source::Client;
 use std::io::{stdout, Write};
+use std::net::TcpStream;
 use std::process::exit;
+use std::time::Duration;
 
 #[derive(Debug, Parser)]
 struct Args {
     #[arg(short, long)]
     password: String,
+    #[arg(index = 2, help = "timeout in milliseconds")]
+    timeout: Option<u64>,
     #[arg(index = 1)]
     server: String,
     #[arg(index = 2)]
@@ -17,18 +21,27 @@ struct Args {
 fn main() {
     env_logger::init();
     let args = Args::parse();
-    let mut client = Client::connect(&args.server).unwrap_or_else(|error| {
+    let tcp_stream = TcpStream::connect(&args.server).unwrap_or_else(|error| {
         error!("{error}");
         exit(1);
     });
+
+    tcp_stream
+        .set_read_timeout(args.timeout.map(Duration::from_millis))
+        .unwrap_or_else(|error| {
+            error!("{error}");
+            exit(2);
+        });
+
+    let mut client: Client = tcp_stream.into();
     let logged_in = client.login(&args.password).unwrap_or_else(|error| {
         error!("{error}");
-        exit(2);
+        exit(3);
     });
     if logged_in {
         let result = client.run(&args.command).unwrap_or_else(|error| {
             error!("{error}");
-            exit(4);
+            exit(5);
         });
         stdout()
             .lock()
@@ -36,6 +49,6 @@ fn main() {
             .expect("Could not write result to stdout.");
     } else {
         error!("Login failed.");
-        exit(3);
+        exit(4);
     }
 }
