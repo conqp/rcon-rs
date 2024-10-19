edfe4c22d671422894adfc7f0ff2c308b31d69e6
Richard Neumann <mail@richard-neumann.de>
2024-10-19 16:28:54 +0200

    Add convenience methods to return strings

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 use std::borrow::Cow;
 use std::fmt::Debug;
+use std::io::{Error, ErrorKind};
 
 #[cfg(feature = "battleye")]
 pub mod battleye;
@@ -16,12 +17,40 @@ pub trait RCon: Debug {
     /// Perform a login.
     ///
     /// # Errors
-    /// Returns an [`std::io::Error`] on errors.
+    ///
+    /// Returns an [`Error`] if any I/O errors occurred or if the login failed.
     fn login(&mut self, password: Cow<'_, str>) -> std::io::Result<bool>;
 
     /// Run a command.
     ///
     /// # Errors
-    /// Returns an [`std::io::Error`] on errors.
+    ///
+    /// Returns an [`Error`] if any I/O errors occurred.
     fn run(&mut self, args: &[Cow<'_, str>]) -> std::io::Result<Vec<u8>>;
+
+    /// Run a command returning a valid UTF-8 string.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`Error`] if any I/O errors occurred or if the returned bytes are not valid UTF-8.
+    fn run_utf8(&mut self, args: &[Cow<'_, str>]) -> std::io::Result<String> {
+        self.run(args).and_then(|bytes| {
+            String::from_utf8(bytes).map_err(|error| Error::new(ErrorKind::InvalidData, error))
+        })
+    }
+
+    /// Run a command returning a valid UTF-8 string.
+    ///
+    /// This command will not error when the returned bytes contain
+    /// invalid UTF-8 bytes, but will replace them accordingly.
+    ///
+    /// See [`String::from_utf8_lossy`] for details.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`Error`] if any I/O errors occurred or if the returned bytes are not valid UTF-8.
+    fn run_utf8_lossy(&mut self, args: &[Cow<'_, str>]) -> std::io::Result<String> {
+        self.run(args)
+            .map(|bytes| String::from_utf8_lossy(&bytes).to_string())
+    }
 }
