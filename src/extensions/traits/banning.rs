662db5ce72d66bc2d5ee2ca431b6136af937817b
Richard Neumann <mail@richard-neumann.de>
2024-10-19 14:36:06 +0200

    Split traits into submodules

---
+++ src/extensions/traits/banning.rs
@@ -0,0 +1,34 @@
+use std::borrow::Cow;
+
+/// Kick a player from the server.
+pub trait Kick {
+    /// Kick a player from the server.
+    ///
+    /// You may specify an optional reason for the kick to forward to the player.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`std::io::Error`] if kicking the player fails.
+    fn kick(&mut self, player: Cow<'_, str>, reason: Option<Cow<'_, str>>) -> std::io::Result<()>;
+}
+
+/// Kick a player from the server.
+pub trait Ban {
+    /// Ban a player from the server.
+    ///
+    /// You may specify an optional reason for the ban to forward to the player.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`std::io::Error`] if banning  the player fails.
+    fn ban(&mut self, player: Cow<'_, str>, reason: Option<Cow<'_, str>>) -> std::io::Result<()>;
+}
+
+pub trait Unban {
+    /// Remove a player ban from the server.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`std::io::Error`] if unbanning  the player fails.
+    fn unban(&mut self, player: Cow<'_, str>) -> std::io::Result<()>;
+}
---
+++ src/extensions/traits/players.rs
@@ -0,0 +1,85 @@
+use crate::{PlayersMut, RCon};
+use std::borrow::Cow;
+use std::fmt::Debug;
+use std::net::{IpAddr, SocketAddr};
+use std::str::FromStr;
+use std::time::Duration;
+use uuid::Uuid;
+
+/// List players on the server.
+pub trait Players {
+    /// The player type that is being returned.
+    type Player: Player;
+
+    /// List players on the server.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`std::io::Error`] if listing the players fails.
+    fn players(&mut self) -> std::io::Result<Vec<Self::Player>>;
+
+    /// Returns an iterator over player proxies.
+    ///
+    /// Each player proxy also implements [`Player`] but also some other functionalities,
+    /// depending on the traits that the underlying RCON client implements.
+    ///
+    /// This can be used to call methods on players directly, such as messaging,
+    /// kicking and banning.
+    ///
+    /// Note that this method does not make any guarantees on the validity of the player information,
+    /// which may or may not change, while the proxy object is held, so use this with care.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`std::io::Error`] if listing the players fails.
+    fn players_mut(&mut self) -> std::io::Result<PlayersMut<'_, Self>>
+    where
+        Self: RCon + Sized,
+    {
+        self.players().map(|players| PlayersMut::new(self, players))
+    }
+}
+
+/// Information about a player.
+pub trait Player: Debug {
+    /// Returns the player's ID.
+    ///
+    /// This is the only mandatory method of `Player` and may return
+    /// the player's name or the string representation of a numeric ID.
+    ///
+    /// Its return value shall be a value that can be used to securely identify the player.
+    fn id(&self) -> Cow<'_, str>;
+
+    /// Returns the player's ID.
+    fn numeric_id(&self) -> Option<i64> {
+        i64::from_str(self.name().as_ref()).ok()
+    }
+
+    /// The player's descriptive name.
+    ///
+    /// This defaults to the return value of [`id`](Self::id) but unlike the
+    /// latter this method should not be used to safely identify players.
+    fn name(&self) -> Cow<'_, str> {
+        self.id()
+    }
+
+    /// Returns the player's UUID.
+    fn uuid(&self) -> Option<Uuid> {
+        None
+    }
+
+    /// Returns the player's socket address.
+    fn socket_addr(&self) -> Option<SocketAddr> {
+        None
+    }
+
+    /// Returns the player's IP address.
+    fn ip_add(&self) -> Option<IpAddr> {
+        self.socket_addr().map(|addr| addr.ip())
+    }
+
+    /// Returns the player's RTT (aka "ping").
+    fn rtt(&self) -> Option<Duration> {
+        None
+    }
+}
