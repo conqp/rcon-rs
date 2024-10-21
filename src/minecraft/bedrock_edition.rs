8d62695e5699cbc183e378a15d5630fae41e7e60
Richard Neumann <mail@richard-neumann.de>
2024-10-21 18:33:29 +0200

    Implement daylock and alwaysday

--- src/minecraft/bedrock_edition.rs
+++ src/minecraft/bedrock_edition.rs
@@ -1,6 +1,32 @@
-//! TODO: implement bedrock edition extensions.
+//! `RCON` client extensions for the `Minecraft: Bedrock Edition`.
 
+use std::future::Future;
+
+use crate::minecraft::util::parse_response;
 use crate::Minecraft;
 
 /// Extension trait for `Source RCON` clients for the `Minecraft: Bedrock Edition`.
-pub trait BedrockEdition: Minecraft {}
+pub trait BedrockEdition: Minecraft {
+    /// Locks and unlocks eternal daytime.
+    fn day_lock(&mut self, lock: bool) -> impl Future<Output = std::io::Result<String>> + Send;
+
+    /// Locks and unlocks eternal daytime.
+    fn always_day(&mut self, lock: bool) -> impl Future<Output = std::io::Result<String>> + Send;
+}
+
+impl<T> BedrockEdition for T
+where
+    T: Minecraft + Send,
+{
+    async fn day_lock(&mut self, lock: bool) -> std::io::Result<String> {
+        self.run_utf8(&["daylock".into(), lock.to_string().into()])
+            .await
+            .and_then(parse_response)
+    }
+
+    async fn always_day(&mut self, lock: bool) -> std::io::Result<String> {
+        self.run_utf8(&["alwaysday".into(), lock.to_string().into()])
+            .await
+            .and_then(parse_response)
+    }
+}
