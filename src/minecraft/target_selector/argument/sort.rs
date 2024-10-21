76796db31baef941a0879e8f59020927139a5f4d
Richard Neumann <mail@richard-neumann.de>
2024-10-21 16:33:09 +0200

    [WIP] Add missing units

---
+++ src/minecraft/target_selector/argument/sort.rs
@@ -0,0 +1,32 @@
+use std::fmt::{Display, Formatter};
+
+/// Sorting strategy.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Sort {
+    /// Sort by increasing distance.
+    Nearest,
+    /// Sort by decreasing distance.
+    Furthest,
+    /// Sort randomly.
+    ///
+    /// Default for @r.
+    Random,
+    /// Do not sort.
+    ///
+    /// This will often return the oldest entities first due to how the game stores
+    /// entities internally, but no order is guaranteed.
+    ///
+    /// Default for @e, @a.
+    Arbitrary,
+}
+
+impl Display for Sort {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Nearest => write!(f, "nearest"),
+            Self::Furthest => write!(f, "furthest"),
+            Self::Random => write!(f, "random"),
+            Self::Arbitrary => write!(f, "arbitrary"),
+        }
+    }
+}
