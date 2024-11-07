83b4ba431795e975f69cee3d97af7b634c89922e
Richard Neumann <mail@richard-neumann.de>
2024-11-07 21:24:51 +0100

    Add ban_ip()

--- src/minecraft/java_edition.rs
+++ src/minecraft/java_edition.rs
@@ -11,6 +11,7 @@ pub use target_selector::{Argument, Sort, TargetSelector};
 
 pub mod advancement;
 pub mod attribute;
+pub mod ban_ip;
 pub mod target_selector;
 
 /// Extension trait for `Source RCON` clients for Minecraft: Java Edition servers.
@@ -60,6 +61,32 @@ pub trait JavaEdition: Minecraft {
         Self: Send,
     {
         let mut args = vec!["ban".into(), target.serialize()];
+
+        if let Some(reason) = reason {
+            args.push(reason);
+        }
+
+        async move { self.run_utf8(&args).await }
+    }
+
+    /// Adds IP address to banlist.
+    ///
+    /// Specifies the IP address to be added to the blacklist.
+    /// Can also be a name of an online player, which represents the IP of that player.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`std::io::Error`] if any I/O errors occur
+    /// or if the returned bytes are not valid UTF-8.
+    fn ban_ip(
+        &mut self,
+        target: ban_ip::Target,
+        reason: Option<Cow<'_, str>>,
+    ) -> impl Future<Output = std::io::Result<String>> + Send
+    where
+        Self: Send,
+    {
+        let mut args = vec!["ban_ip".into(), target.serialize()];
 
         if let Some(reason) = reason {
             args.push(reason);
---
+++ src/minecraft/java_edition/ban_ip.rs
@@ -0,0 +1,37 @@
+//! Data structures related to IP banning.
+
+use std::borrow::Cow;
+use std::net::IpAddr;
+
+use crate::minecraft::java_edition::TargetSelector;
+use crate::minecraft::{Entity, Serialize};
+
+/// A target for IP banning.
+///
+/// Can either be an IP address or an entity.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Target {
+    Ip(IpAddr),
+    Entity(Entity<TargetSelector>),
+}
+
+impl From<IpAddr> for Target {
+    fn from(ip: IpAddr) -> Target {
+        Self::Ip(ip)
+    }
+}
+
+impl From<Entity<TargetSelector>> for Target {
+    fn from(entity: Entity<TargetSelector>) -> Target {
+        Self::Entity(entity)
+    }
+}
+
+impl Serialize for Target {
+    fn serialize(self) -> Cow<'static, str> {
+        match self {
+            Self::Ip(ip) => Cow::Owned(ip.to_string()),
+            Self::Entity(entity) => entity.serialize(),
+        }
+    }
+}
